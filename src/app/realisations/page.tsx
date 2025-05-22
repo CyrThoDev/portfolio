bfb185d46a27974bdf568cd510cd672bf3bd5ae4
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2025-05-22 22:31:54 +0200

    responsive relisations

--- src/app/realisations/page.tsx
+++ src/app/realisations/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
 			<h1 className="text-3xl  text-left my-20 font-bold">
 				Un aperçu de mes dernières réalisations...
 			</h1>
-			<div className="space-y-10 grid grid-cols-3 gap-8 mb-20">
+			<div className="space-y-10 grid grid-cols-1 lg:grid-cols-3 gap-8 mb-20">
 				{realisations.map((realisation) => (
 					<RealisationCard key={realisation.title} realisation={realisation} />
 				))}
